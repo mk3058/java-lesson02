8ac04661bb0d14541f0f71e1531de54b9a8f8566
mk3058 <kmk3058@icloud.com>
2024-03-21 13:17:55 +0900

    ForWhileIteration 솔루션 추가

--- src/main/java/kr/easw/lesson2/ForWhileIteration.java
+++ src/main/java/kr/easw/lesson2/ForWhileIteration.java
@@ -1,6 +1,7 @@
 package kr.easw.lesson2;
 
 public class ForWhileIteration {
+
     private static final int TARGET_ITERATION = 30;
 
     private static int currentIteration = 0;
@@ -16,20 +17,23 @@ public class ForWhileIteration {
     }
 
     /**
-     *  해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-     *  for문을 이용해, 정확히 10번 {@code runTask} 메서드를 실행해야 합니다.
+     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 : for문을 이용해, 정확히 10번 {@code runTask} 메서드를 실행해야 합니다.
      */
     private static void iterateTenTime() {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        for (int i = 0; i < 10; i++) {
+            runTask();
+        }
     }
 
     /**
-     *  해당 메서드는 다음과 같은 역할을 가져야 합니다 :
-     *  while문을 이용해, 정확히 20번 {@code runTask} 메서드를 실행해야 합니다.
+     * 해당 메서드는 다음과 같은 역할을 가져야 합니다 : while문을 이용해, 정확히 20번 {@code runTask} 메서드를 실행해야 합니다.
      */
     private static void iterateTwentyTime() {
         int iterateCounter = 0;
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        while (iterateCounter < 20) {
+            runTask();
+            iterateCounter++;
+        }
     }
 
     private static void runTask() {
